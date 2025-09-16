2c7312890f60a0dfd9e42d00203a1c4210a31543
Jad kobeissi <jadkoneissi@gmail.com>
2025-09-16 16:35:56 +0300

    initial setup + landing page hero about section

--- app/page.tsx
+++ app/page.tsx
@@ -1,103 +1,59 @@
-import Image from "next/image";
-
-export default function Home() {
+function Home() {
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="font-mono list-inside list-decimal text-sm/6 text-center sm:text-left">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] font-mono font-semibold px-1 py-0.5 rounded">
-              app/page.tsx
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div
+      id="home"
+      className="flex landing-md:flex-row flex-col-reverse items-center justify-between landing-md:px-[2rem] w-screen mt-[15vh]"
+    >
+      <div className="flex flex-col items-start justify-center landing-md:min-h-screen py-4 landing-md:w-1/2 w-screen">
+        <h1 className="text-[3rem] font-bold text-left">Welcome To Bacnotes</h1>
+        <p className="text-[#d9d9d9cc]">
+          Share, discover, and collaborate on notes with your classmates. This
+          app is BAC students’ private space to upload notes, access helpful
+          resources, and support each other throughout the school year.
+        </p>
+        <div className="flex gap-4 items-center justify-center mt-4">
+          <button className="text-[1.5rem] rounded-xl bg-[#1C6CA0] font-bold px-4 py-2 hover:bg-[#145a86] whitespace-nowrap">
+            Get Started Now
+          </button>
+          <div className="hidden landing-md:block">
+            <h1 className="font-bold text-[2rem]">OR</h1>
+            <button className="text-[1.5rem] rounded-xl bg-[#141414] font-bold px-6 py-2">
+              LogIn
+            </button>
+          </div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+      </div>
+      <div className="landing-md:w-1/2 flex justify-end">
+        <img src="/bac.png" className="w-[30rem] rounded-xl" />
+      </div>
     </div>
+  );
+}
+function About() {
+  return (
+    <div
+      id="about"
+      className="flex flex-col items-center justify-center landing-md:px-[2rem] w-screen mt-[20vh]"
+    >
+      <h1 className="text-[3rem] font-bold">What Is Bacnotes?</h1>
+      <p className="text-[#d9d9d9] text-center">
+        <span className="text-[#1C6CA0] font-bold">Bacnotes</span> is a platform
+        designed exclusively for BAC students, allowing you to{" "}
+        <span className="text-[#1C6CA0] font-bold">share</span> notes and{" "}
+        <span className="text-[#1C6CA0] font-bold">stay connected</span> through
+        a familiar, secure space. Whether you’re contributing your own notes or
+        finding notes from classmates,{" "}
+        <span className="text-[#1C6CA0] font-bold">Bacnotes</span> helps bring
+        the student community closer together throughout the school year.
+      </p>
+    </div>
+  );
+}
+export default function Page() {
+  return (
+    <>
+      <Home />
+      <About />
+    </>
   );
 }
